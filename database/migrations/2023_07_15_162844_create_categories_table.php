8fc84dda9a0d984e8cda7ea7746c4860caf64e94
Newsplot <110198422+Raul767@users.noreply.github.com>
2023-07-17 01:02:49 -0500

    Update 2023_07_15_162844_create_categories_table.php

--- database/migrations/2023_07_15_162844_create_categories_table.php
+++ database/migrations/2023_07_15_162844_create_categories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre', 50);
+            $table->text('descripcion');
             $table->timestamps();
         });
     }
