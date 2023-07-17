38cd0fcdeb9a7af7afd2093aceb8b6f35a9073ba
Newsplot <110198422+Raul767@users.noreply.github.com>
2023-07-17 01:04:10 -0500

    Update 2023_07_15_162917_create_empleados_table.php

--- database/migrations/2023_07_15_162917_create_empleados_table.php
+++ database/migrations/2023_07_15_162917_create_empleados_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('empleados', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre', 50);
+            $table->string('apellido', 50);
+            $table->string('direccion', 100);
+            $table->char('telefono', 10);
+
+            $table->bigInteger('idCiudad')->unsigned();
+
+            $table->foreign('idCiudad')->references('id')->on('ciudad')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
