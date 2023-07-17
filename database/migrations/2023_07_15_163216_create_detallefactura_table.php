1041e3ed993855057c30f2656b8e6222b5bd4393
Newsplot <110198422+Raul767@users.noreply.github.com>
2023-07-17 01:06:07 -0500

    Update 2023_07_15_163216_create_detallefactura_table.php

--- database/migrations/2023_07_15_163216_create_detallefactura_table.php
+++ database/migrations/2023_07_15_163216_create_detallefactura_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('detallefactura', function (Blueprint $table) {
             $table->id();
+            $table->string('nombredelatienda');
+            $table->decimal('precio', 10,2);
+            $table->char('cantidad', 10);
+
+            $table->bigInteger('idProductos')->unsigned();
+            $table->bigInteger('idFactura')->unsigned();
+
+            $table->foreign('idProductos')->references('id')->on('productos')->onDelete('cascade');
+            $table->foreign('idFactura')->references('id')->on('facturas')->onDelete('cascade');
             $table->timestamps();
         });
     }
