3ff3afe0e701562ebfc0de60c5b85e1e0baaf5c9
Newsplot <110198422+Raul767@users.noreply.github.com>
2023-07-17 01:01:57 -0500

    Update 2023_07_15_162659_create_departamento_table.php

--- database/migrations/2023_07_15_162659_create_departamento_table.php
+++ database/migrations/2023_07_15_162659_create_departamento_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('departamento', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre', 30);
             $table->timestamps();
         });
     }
